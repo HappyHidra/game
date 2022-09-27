f9706526afb8a4235c7cae3e3f4ee5f9ae01fc40
DeepReap <okmmiha@mail.ru>
2022-09-28 01:30:34 +0300

    changed constants.php for local dev

--- includes/constants.php
+++ includes/constants.php
@@ -1,10 +1,4 @@
 <?php
-// Константы базы данных для сервера лайт хост
-// define("DB_SERVER", "localhost");
-// define("DB_USER", "tuocixdj_admin");
-// define("DB_PASS", "123456");
-// define("DB_NAME", "tuocixdj_game");
-
 // // Константы базы данных для XAMPP 
 define("DB_SERVER", "localhost");
 define("DB_USER", "root");
